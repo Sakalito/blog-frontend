e7a69e251b9af6ec1bc8b7b2c28dbece6b6aaac1
Sakalito <wassim.houiat@gmail.com>
2022-05-13 19:27:45 +0200

    mise en propre de la structure du proejt & tests pour la page article

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,35 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 import axios from 'axios';
 import { onMounted, reactive } from 'vue';
 import HomePage from './pages/home/HomePage.vue';
 import PostPage from './pages/post/PostPage.vue';
-import index from './index.css';
 
 //Creation de la requete
-const httpClient = axios.create({ baseURL: 'http://localhost3001:' })
-//met blog en état reactif 
+const httpClient = axios.create({ baseURL: 'http://localhost3001:' });
+//met blog en état reactif
 const state = reactive({
-  blog: ''
-})
+  blog: '',
+});
 //appele les requetes
 onMounted(async () => {
-  const message = await httpClient.get('/')
-  console.log(message)
-  const blog = await httpClient.get('/blog')
-  console.log(blog)
-  console.log(blog.data)
-  state.blog = blog.data
+  const message = await httpClient.get('/');
+  console.log(message);
+  const blog = await httpClient.get('/blog');
+  console.log(blog);
+  console.log(blog.data);
+  state.blog = blog.data;
 });
-
 </script>
 
 <template>
-<!-- retourne les resultats-->
-
-<router-link to="/" class="text-red-500">Accueil</router-link>
-<router-link to="/article">Article</router-link>
-<router-view />
-
+  <!-- retourne les resultats-->
+  <router-link to="/" class="font-bold text-red-500">Accueil</router-link>
+  <router-link to="/article">Article</router-link>
+  <br />
+  <router-view />
 </template>
 
 <style>
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   root: 'src',
+  publicDir: 'src/assets',
   plugins: [vue()]
 
 })
